ddebcf6fd6e66d5a0f5b6549de83e566f8e577b9
Merlin Marek <merlin_marek@web.de>
2018-01-26 16:38:57 +0100

    added logging to file and improved documentation

--- logger.c
+++ logger.c
@@ -1,15 +1,21 @@
+#include <errno.h>
+#include <fcntl.h>
 #include <libgen.h>
 #include <limits.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
-#include <pthread.h>
+#include <time.h>
+#include <unistd.h>
 #include <sys/time.h>
 
 #include "logger.h"
 
 static pthread_mutex_t logger_lock;
 static log_level_t current_log_level = LOG_DEBUG;
+static int log_file = -1;
+static int log_file_creation_failed = 0; // if we faile to create a log file we do not want to retry so the command line output is not flooded with error messages
 
 static const char* log_level_strings[] = {
     "DEBUG",
@@ -27,6 +33,11 @@ void destroy_logger_lock() {
 	if(pthread_mutex_destroy(&logger_lock) != 0) {
 		printf("pthread_mutex_destroy failed\n");
 	}
+
+	// this probably should be somewhere else but this is easiest for now
+	if(log_file != -1) {
+		close(log_file);
+	}
 }
 
 void set_log_level(log_level_t level) {
@@ -37,6 +48,20 @@ void set_log_level(log_level_t level) {
 
 void logger(log_level_t level, const char* file, const char* function, const char* format_string, ...) {
 	pthread_mutex_lock(&logger_lock);
+	if(log_file == -1 && log_file_creation_failed == 0) {
+		// we need to open a new log file
+		// first generate a file name from the current date
+		char date_buffer[128];
+		strcpy(date_buffer, "./log/");
+		time_t t = time(NULL);
+		struct tm tm = *localtime(&t);
+        strftime(date_buffer + strlen(date_buffer), sizeof(date_buffer) - strlen(date_buffer), "%d_%m_%Y_%a_%T", &tm);
+        log_file = open(date_buffer, O_RDWR|O_CREAT, 0664);
+        if(log_file == -1) {
+        	printf("The log file could not be created: %s\nNo logs will be written.\n", strerror(errno));
+        	log_file_creation_failed = 1; // so we do not retry...
+        }
+	}
 	if(level >= current_log_level) {
         va_list arguments;
         va_start(arguments, format_string);
@@ -54,6 +79,18 @@ void logger(log_level_t level, const char* file, const char* function, const cha
         gettimeofday(&now, NULL);
         struct tm* now_local = localtime(&now.tv_sec);
         strftime(timestamp_buffer, sizeof(timestamp_buffer), "%d.%m %T", now_local);
+
+        if(log_file != -1) {
+            if(level > LOG_ERROR) {
+                dprintf(log_file, "[%-14s %-7s %-20s %-25s] ", timestamp_buffer, "INVALID", basename(file_path_buffer), function);
+            } else {
+                dprintf(log_file, "[%-14s %-7s %-20s %-25s] ", timestamp_buffer, log_level_strings[level], basename(file_path_buffer), function);
+            }
+            vdprintf(log_file, format_string, arguments);
+            fsync(log_file); // flush so we do not lose any data if the application crashes
+            va_end(arguments);
+            va_start(arguments, format_string);
+        }
 
         if(level > LOG_ERROR) {
         	printf("[%-14s %-7s %-20s %-25s] ", timestamp_buffer, "INVALID", basename(file_path_buffer), function);
